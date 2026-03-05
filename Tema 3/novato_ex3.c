615132dd6ce3356e826abd036462a638c57b36bb
Estacio202310300133 <202310300133@alunos.estacio.br>
2026-03-05 21:04:46 +0000

    Comparando temperaturas

---
+++ Tema 3/novato_ex3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    // Comparando temperaturas
+    
+    float temperatura = 32.0;
+    
+    if (temperatura > 30.0) {
+        printf("Está calor\n");
+    }
+
+    return 0;
+    
+}
