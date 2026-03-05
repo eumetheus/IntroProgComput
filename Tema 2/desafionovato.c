7afcd377587557428814e60a59e867b5ea092b59
Estacio202310300133 <202310300133@alunos.estacio.br>
2026-03-05 01:44:02 +0000

    Tema2: Desafio Novato - Super trunfo - Variáveis

---
+++ Tema 2/desafionovato.c
@@ -0,0 +1,32 @@
+//--------------------------------------------------------------
+// Desafio Novato do tema 2 :Criando as Cartas do Super Trunfo
+//--------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // Declarando variáveis:
+    // uf -> estado, cod -> código do estado , cid -> cidade
+
+    char uf1[3], cod1[10], cid1[50];
+    char uf2[3], cod2[10], cid2[50];
+
+    // Populacao,Área e PIB de cada cidade
+
+    int   pop1,  pop2;
+    float area1, area2;
+    float pib1,  pib2;
+
+    // Pontuação de cada cidade
+
+    int pnt1, pnt2;
+
+    // Densidade e PIB per Capita
+
+    float den1, den2;
+    float pibc1, pibc2;
+
+    return 0;
+}
