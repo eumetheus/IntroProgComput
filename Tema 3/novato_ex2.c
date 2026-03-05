b6fe3d47e28691a7b9e2523498951959eebd7a2b
Estacio202310300133 <202310300133@alunos.estacio.br>
2026-03-05 20:58:47 +0000

    Atualizando ex2

---
+++ Tema 3/novato_ex2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    // Comparandoo idade
+
+    int idade = 20;
+
+    if (idade >= 18) {
+        printf("Você é maior de idade\n");
+    }
+
+    return 0;
+}
