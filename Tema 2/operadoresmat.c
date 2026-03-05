5f7f3acd35973081dca46697c5200321fbf034e0
Estacio202310300133 <202310300133@alunos.estacio.br>
2026-03-05 03:00:30 +0000

    Tema 2 - Operadore Matemáticos - Final

--- Tema 2/operadoresmat.c
+++ Tema 2/operadoresmat.c
@@ -19,6 +19,37 @@ int main() {
     int subtracao = a - b;
     int multiplicacao = a * b;
     int divisao = a / b;
+
+    // Guardando o valor original de A e B
+
+    int aOri = a;
+    int bOri = b;
+
+    // Operadores de atribuição de valores
+
+    a += 2; // a valerá 20 + 2 = 22
+    b *= 3; // b valerá 10 * 3 = 30
+ 
+    // Operadores de incremento e decremento
+
+    a++; // a valerá 22 + 1 = 23
+    b--; // b valerá 30 - 1 = 29
+ 
+    // Exibindo os resultados das transformações das variáveis
+    //-----------------------------------------------------------
+
+    printf("Valor original de A: %d\n",aOri);
+    printf("Valor original de B: %d\n",bOri);
+    printf(" \n");
+
+    printf("Soma: %d\n", soma);
+    printf("Subtração: %d\n", subtracao);
+    printf("Multiplicação: %d\n", multiplicacao);
+    printf("Divisão: %d\n", divisao);
+    
+    printf(" \n");
+    printf("Novo valor de a (após += 2 e ++): %d\n", a);
+    printf("Novo valor de b (após *= 3 e --): %d\n", b);
  
     return 0;
 
