da904ebe660477141aefedc3d6fa2acda77eeb73
Estacio202310300133 <202310300133@alunos.estacio.br>
2026-03-05 02:54:29 +0000

    tema 2 : Operadores Matemáticos

---
+++ Tema 2/operadoresmat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Função Principal
+
+int main() {
+
+    // Declarando variáveis
+
+    int a = 20;
+    int b = 10;
+ 
+    // Operadores aritméticos
+    // Soma -> +
+    // Subtração -> -
+    // Multiplicação -> *
+    // Divisão -> /
+
+    int soma = a + b;
+    int subtracao = a - b;
+    int multiplicacao = a * b;
+    int divisao = a / b;
+ 
+    return 0;
+
+}
