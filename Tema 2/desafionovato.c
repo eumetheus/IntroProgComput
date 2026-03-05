63836dffe30821838123268cf937cb36a8267c09
Estacio202310300133 <202310300133@alunos.estacio.br>
2026-03-05 02:06:24 +0000

    Tema 2 - Desafio Novato - Leitura dos dados da carta 1

--- Tema 2/desafionovato.c
+++ Tema 2/desafionovato.c
@@ -10,8 +10,8 @@ int main() {
     // Declarando variáveis:
     // uf -> estado, cod -> código do estado , cid -> cidade
 
-    char uf1[3], cod1[10], cid1[50];
-    char uf2[3], cod2[10], cid2[50];
+    char uf1[1], cod1[2], cid1[20];
+    char uf2[1], cod2[2], cid2[20];
 
     // Populacao,Área e PIB de cada cidade
 
@@ -19,14 +19,66 @@ int main() {
     float area1, area2;
     float pib1,  pib2;
 
-    // Pontuação de cada cidade
+    // Número de Pontos turísticos de cada cidade
 
     int pnt1, pnt2;
 
-    // Densidade e PIB per Capita
+    
+    // Lendo os dados da primeira carta:
 
-    float den1, den2;
-    float pibc1, pibc2;
+    printf("Digite a seguir os dados da Carta 1: \n");
+    printf("------------------------------------\n\n");
+
+    printf("Estado (A a H): \n");
+    scanf("%s",uf1);
+
+    printf("Código da Carta: (01 a 04) \n");
+    scanf("%s",cod1);
+
+    printf("Cidade (20 caracteres) \n");
+    scanf("%s",cid1);
+
+    printf("População: \n");
+    scanf("%d",&pop1);
+
+    printf("Área em Km: \n");
+    scanf("%f",&area1);
+
+    printf("PIB: \n");
+    scanf("%f",&pib1);
+
+    printf("Quantidade de Pontos Turísticos: \n");
+    scanf("%d",&pnt1);
+
+
+    // Lendo os dados da segunda  carta:
+
+    printf("Digite a seguir os dados da Carta 1: \n");
+    printf("------------------------------------\n\n");
+
+    printf("Estado (A a H): \n");
+    scanf("%s",uf1);
+
+    printf("Código da Carta: (01 a 04) \n");
+    scanf("%s",cod1);
+
+    printf("Cidade (20 caracteres) \n");
+    scanf("%s",cid1);
+
+    printf("População: \n");
+    scanf("%d",&pop1);
+
+    printf("Área em Km: \n");
+    scanf("%f",&area1);
+
+    printf("PIB: \n");
+    scanf("%f",&pib1);
+
+    printf("Quantidade de Pontos Turísticos: \n");
+    scanf("%d",&pnt1);
+
 
     return 0;
+
+
 }
