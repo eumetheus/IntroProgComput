66fe8c45ca7cf148e9f0b6394ac250f40a3dfddd
Estacio202310300133 <202310300133@alunos.estacio.br>
2026-03-05 04:03:32 +0000

    Long Int e Long Double

--- Tema 2/longint.c
+++ Tema 2/longint.c
@@ -5,13 +5,13 @@ int main() {
     // Variáveis com modificador long
 
     int numRegular = 2147483647;        // Valor máximo de int
-    long int numGrande  = 2147483647;   // Valor máximo de int, mas com o modificador long pode ir até 9,223,372,036,854,775,807
+    long long int numGrande  = 2147483647;   // Valor máximo de int, mas com o modificador long pode ir até 9,223,372,036,854,775,807
  
     printf("Número regular (int): %d\n", numRegular);
-    printf("Número grande (long int): %ld\n", numGrande);
+    printf("Número grande (long int): %lld\n", numGrande);
  
-    numGrande = 2147483648; // Valor maior que o máximo de int
-    printf("Número grande atualizado (long int): %ld\n", numGrande);
+    numGrande = 214748364700; // Valor maior que o máximo de int
+    printf("Número grande atualizado (long int): %lld\n", numGrande);
  
     return 0;
 }
