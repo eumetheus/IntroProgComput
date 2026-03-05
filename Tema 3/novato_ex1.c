0dc8a72edadca1c7f93043e645b37752f7b08e0d
Estacio202310300133 <202310300133@alunos.estacio.br>
2026-03-05 20:55:08 +0000

    Ex1 Novato

---
+++ Tema 3/novato_ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    // Variáveis
+
+    int numero1 = 8;
+    int numero2 = 5;
+
+    // Comparando os números
+
+    if (numero1 > numero2) {
+        printf("numero1 é maior que numero2\n");
+    }
+
+    return 0;
+}
